e1993f24bc61b2487df5ee07158c7bc10e4d5dae
Jini <yjkwon424@gmail.com>
2025-04-13 00:26:26 +0900

    Initial Commit

---
+++ MoQoo/MoQooApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoQooApp.swift
+//  MoQoo
+//
+//  Created by Jini on 4/13/25.
+//
+
+import SwiftUI
+
+@main
+struct MoQooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
