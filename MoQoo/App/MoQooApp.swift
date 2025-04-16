76023b9dda316aaab6382b2a1bb1b27dd2cd51bd
Jini <yjkwon424@gmail.com>
2025-04-17 01:27:09 +0900

    Feat: Firebase 연동

--- MoQoo/App/MoQooApp.swift
+++ MoQoo/App/MoQooApp.swift
@@ -6,12 +6,38 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct MoQooApp: App {
-    var body: some Scene {
-        WindowGroup {
-            TabBarView()
-        }
+  // register app delegate for Firebase setup
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+          TabBarView()
+      }
     }
+  }
 }
+
+//@main
+//struct MoQooApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            TabBarView()
+//        }
+//    }
+//}
